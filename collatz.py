b01e1d7bbd8a62eb462003457138de94556bf25d
Krishna K Lalwani <krishnakashab@gmail.com>
2023-06-26 20:34:06 +0530

    Initial Commit

--- collatz.py
+++ collatz.py
@@ -1,76 +1,44 @@
-import os
 import openpyxl
-import sys
 
-add = 166440
-# start = 2**70+(add*24)
-# end = start+add 
-
-start = 1
-end = 2**20
-
-def collatz(x):
-    sequence = [x]
-    while x != 1:
-        if x % 2 == 0:
-            x //= 2
+def collatz_steps(n):
+    steps = 0
+    while n != 1:
+        if n % 2 == 0:
+            n = n // 2
         else:
-            x = 3 * x + 1
-        sequence.append(x)
-    return sequence
+            n = 3 * n + 1
+        steps += 1
+    return steps
 
-def save_to_excel(start, end):
-    filename = f"Excels/collatz {start} - {end}.xlsx"
-
-    if not os.path.exists("Excels"):
-        os.makedirs("Excels")
-
-    if not os.path.exists(filename):
-        workbook = openpyxl.Workbook()
-        workbook.save(filename)
-
-    workbook = openpyxl.load_workbook(filename)
-    worksheet = workbook.active
-
-    row = worksheet.max_row + 1
-
-    for num in range(start, end + 1):
-        sequence = collatz(num)
-
-        worksheet.cell(row=row, column=1).value = num
-        for col, value in enumerate(sequence, start=2):
-            worksheet.cell(row=row, column=col).value = value
+def write_to_excel(filename, data):
+    wb = openpyxl.load_workbook(filename)
+    sheet = wb.active
+    row = 1
+    while sheet.cell(row=row, column=1).value is not None:
         row += 1
+    sheet.cell(row=row, column=1).value = data[0]
+    sheet.cell(row=row, column=2).value = data[1]
+    wb.save(filename)
 
-        print(f"::warning file=collatz.py,line=45::Number {num} saved to the Excel file.")
-        sys.stdout.flush()
+def collatz_to_excel(filename, start_num, max_rows):
+    try:
+        wb = openpyxl.load_workbook(filename)
+    except FileNotFoundError:
+        wb = openpyxl.Workbook()
+        wb.active.title = "Sheet1"
 
-    workbook.save(filename)
+    sheet = wb.active
+    current_row = 1
+    while sheet.cell(row=current_row, column=1).value is not None:
+        current_row += 1
 
-def save_step_counter(start, end):
-    directory = "Steps"
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+    for num in range(start_num, start_num + max_rows):
+        steps = collatz_steps(num)
+        write_to_excel(filename, (num, steps))
+        current_row += 1
 
-    filename = f"{directory}/collatz {start} - {end}.txt"
-    if not os.path.exists(filename):
-        with open(filename, 'w') as file:
-            file.write(f"Step counter for numbers {start} to {end}:\n\n")
-    else:
-        print("File already exists. Skipping creation.")
+    wb.save(filename)
+    wb.close()
 
-    for i in range(start, end + 1):
-        sequence = collatz(i)
-        steps = len(sequence) - 1
-        with open(filename, 'a') as file:
-            file.write(f"Number: {i}\n")
-            file.write(f"Steps: {steps}\n")
-
-        print(f"{i}")
-        sys.stdout.flush()
-
-def main():
-    save_step_counter(start, end)
-    save_to_excel(start, end)
-
-main()
+# Example usage
+collatz_to_excel("collatz_steps.xlsx", 1, 1048576)
