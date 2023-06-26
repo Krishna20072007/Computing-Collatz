d93243e3d96be462111f7fb1af472253a7c9cb54
Krishna K Lalwani <krishnakashab@gmail.com>
2023-06-26 20:46:14 +0530

    Initial Commit

--- collatz.py
+++ collatz.py
@@ -48,5 +48,5 @@ def collatz_to_excel(filename, start_num, max_rows, start, end):
 
 # Example usage
 start = 1
-end = 100000
+end = 2**20
 collatz_to_excel("Excels/collatz_steps.xlsx", 1, 100, start, end)
