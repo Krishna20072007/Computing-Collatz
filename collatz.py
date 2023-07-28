e9f08c64d897a2436895d0060fd3b8955883fbb2
Krishna K Lalwani <krishnakashab@gmail.com>
2023-07-28 16:59:55 +0530

    Initial Commit

--- collatz.py
+++ collatz.py
@@ -43,7 +43,7 @@ def collatz_to_excel(filename, start, end):
 
     print("All numbers written!")
 
-start = 24_30_000
+start = 24_40_000
 end = start + 10000 
 start_time = time.time()
 collatz_to_excel(f"Excels/collatz_steps {start} to {end}.xlsx", start, end)
